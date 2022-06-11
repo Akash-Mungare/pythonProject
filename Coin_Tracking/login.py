d89e96ea22d8736c57ea51e68ad424c0cc541f47
AkashMungare4420 <akashmungare@gmail.com>
2022-06-12 01:03:00 +0530

    first commit

---
+++ Coin_Tracking/login.py
@@ -0,0 +1,21 @@
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome(executable_path="C:\\Selenium\chromedriver.exe")
+# driver.get("https://ct.sensegiz.com/login")
+# driver.maximize_window()
+# print(driver.title)
+# driver.close()
+
+
+def addition(x,y,z):
+    """
+    This is function
+    :param x:
+    :param y:
+    :param z:
+    :return:
+    """
+    return x+y+z
+
+z = addition(55,58,61)
+print(z)
---
+++ single_data.py
@@ -0,0 +1,43 @@
+from random import *
+import datetime
+from datetime import timedelta
+
+f = open("SingleCoinData.txt", "w")
+gateway = "1234567890AB"
+coin = (str(1)).zfill(4)
+# coin = coin1
+# print("======================== coin ======================", coin)
+find = (str(1)).zfill(4)
+# find = find1
+# print("======================== find ======================", find)
+rssi1 = ('A3','A4','A5','A6','A7','A8','A9','AA','AB','AC','AD','AE','AF','B0','B1','B2','B3','B4','B5','B6','B7','B8','B9','BA','BB','BC','BD','BE','BF','C0','C1','C2')
+rssi = choice(rssi1)
+# print("======================== rssi ======================", rssi)
+# UTCTime = datetime.datetime.now() - 19800000
+utcTime = datetime.datetime.now(datetime.timezone.utc)
+utc = utcTime.strftime("%Y-%m-%d %H:%M:%S")
+x=60
+j=4
+for i in range(x):
+    diffTime = utcTime - timedelta(hours=4, minutes=26, seconds=-j)
+    # print("======================== now()======================", diffTime)
+    dd = ((hex(utcTime.day).upper()).removeprefix('0X')).zfill(2)
+    # dd = utcdd.zfill(2)
+    # print("======================== DD ======================", dd)
+    hh = ((hex(diffTime.hour).upper()).removeprefix('0X')).zfill(2)
+    mm = ((hex(diffTime.minute).upper()).removeprefix('0X')).zfill(2)
+    ss = ((hex(diffTime.second).upper()).removeprefix('0X')).zfill(2)
+    # print("dd : ", dd, " hh: ",hh," mm: ",mm," ss: ",ss)
+    dataType = "AF"
+    btr = str(59)
+    data = gateway+coin+find+rssi+dd+hh+mm+ss+dataType+btr
+    print(data,"\n")
+    print("============================length of data===================",len(data))
+    f.write(data + "\n")
+    j+=7
+    continue
+offline = str("03")
+data = gateway+coin+find+offline+dd+hh+mm+ss+dataType+btr
+f.write(data)
+
+f.close()
