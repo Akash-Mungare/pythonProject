333a3f286e941c6d3944906b5bb35673e7335734
AkashMungare4420 <akashmungare@gmail.com>
2022-07-08 18:20:46 +0530

    CSV and Kamdhenu

--- Coin_Tracking/manageDevices.py
+++ Coin_Tracking/manageDevices.py
@@ -1,14 +1,14 @@
 import time
-from xml.dom.pulldom import END_ELEMENT
 from selenium.webdriver.common.by import By
 from xPathLocator import *
 asset = manage_assets()
+driver = web_driver
 
 class manage_Find():
     def add_Assets(self):
         x=10
         for i in range(x):
-            END_ELEMENT(By.XPATH,asset.add_find_btn).click()
+            driver().x(By.XPATH,asset.add_find_btn).click()
             time.sleep(4)
             END_ELEMENT(By.type,asset.add_find).send_keys(x)
             time.sleep(4)
