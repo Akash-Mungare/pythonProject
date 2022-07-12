ac432f5bbe4d900d0a644e1220dbc9d8e755a493
AkashMungare4420 <akashmungare@gmail.com>
2022-07-12 11:56:03 +0530

    Kamdhenu

--- Coin_Tracking/manageDevices.py
+++ Coin_Tracking/manageDevices.py
@@ -2,16 +2,16 @@ import time
 from selenium.webdriver.common.by import By
 from xPathLocator import *
 asset = manage_assets()
-driver = web_driver
+# driver = web_driver
 
 class manage_Find():
     def add_Assets(self):
         x=10
         for i in range(x):
-            driver().x(By.XPATH,asset.add_find_btn).click()
+            web_driver.x(By.XPATH,asset.add_find_btn).click()
             time.sleep(4)
-            END_ELEMENT(By.type,asset.add_find).send_keys(x)
+            web_driver(By.type,asset.add_find).send_keys(x)
             time.sleep(4)
-            END_ELEMENT(By.XPATH,asset.add_find_submit).click()
+            web_driver(By.XPATH,asset.add_find_submit).click()
             time.sleep(4)
             return
