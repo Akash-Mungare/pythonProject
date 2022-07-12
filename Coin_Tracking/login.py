bea07e910329f40b555da70d7e9d02550899e95f
AkashMungare4420 <akashmungare@gmail.com>
2022-07-12 23:59:06 +0530

    add finds

--- Coin_Tracking/login.py
+++ Coin_Tracking/login.py
@@ -11,7 +11,7 @@ cred = credentials()
 class webelement():
     # driver = webdriver.Chrome(executable_path="C:\\Selenium\chromedriver.exe")
     def web_login(self):
-        list = ["1. MSIL Testing", "2.NewAT Testing", "3.CT Server"]
+        list = ["1. NPT Testing", "2.NewAT Testing", "3.CT Testing"]
         print("List of server : ", list, "\n")
 
         print("Enter the value to select server:")
@@ -20,13 +20,15 @@ class webelement():
         driver = webdriver.Chrome(executable_path="C:\\Selenium\chromedriver.exe")
         driver.maximize_window()
         if x == 1:
-            driver.get(xpath_L.mst_baseUrl)
+            driver.get(xpath_L.npt_baseUrl)
             time.sleep(2)
-            driver.find_element(By.ID,xpath_L.user).send_keys(cred.mst_username)
+            driver.find_element(By.ID,xpath_L.user).send_keys(cred.npt_username)
             time.sleep(2)
-            driver.find_element(By.ID,xpath_L.pwd).send_keys(cred.mst_password)
+            driver.find_element(By.ID,xpath_L.pwd).send_keys(cred.npt_password)
             time.sleep(2)
             driver.find_element(By.CLASS_NAME,xpath_L.lSubmit).click()
+            time.sleep(2)
+            manage_device = add_Assets(driver)
             time.sleep(2)
 
         elif x == 2:
@@ -40,8 +42,8 @@ class webelement():
             time.sleep(2)
             driver.find_element(By.CLASS_NAME,xpath_L.lSubmit).click()
             time.sleep(8)
-            # driver.find_element(By.CLASS_NAME,asset.add_find).click()
-            # time.sleep(2)
+            manage_device = add_Assets(driver)
+            time.sleep(2)
         elif x == 3:
             # driver = webdriver.Chrome(executable_path="C:\\Selenium\chromedriver.exe")
             # driver.maximize_window()
--- Coin_Tracking/manageDevices.py
+++ Coin_Tracking/manageDevices.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 from xPathLocator import *
 asset = manage_assets()
 
-# driver = webdriver.Chrome(executable_path="C:\\Selenium\chromedriver.exe")
+# =================== Add Find ==============================
 def add_Assets(driver):
-    for i in range(1,10):
-        driver.find_element(By.XPATH,asset.add_find_btn).click()
-        time.sleep(2)
+    driver.find_element(By.XPATH,asset.add_find_btn).click()
+    time.sleep(2)
+    for i in range(9,11):
         driver.find_element(By.XPATH,asset.add_find).send_keys(i)
         time.sleep(4)
         driver.find_element(By.XPATH,asset.add_find_submit).click()
-        time.sleep(4)
-    return
+        time.sleep(6)
+        
